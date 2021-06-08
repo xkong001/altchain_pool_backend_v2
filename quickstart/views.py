78a9d2a76f4e0af4110151b660ede642b1c74e0e
xkong <xianghe_kong@yahoo.com>
2021-06-08 23:58:06 +0800

    update email gen return to { "code": xxx, "msg": xxx, "data":[] }

--- quickstart/views.py
+++ quickstart/views.py
@@ -22,16 +22,33 @@ def email_code_gen(request):
     """
     Manage request for user account email
     """
-    email_decode = urllib.parse.unquote(request.data.get('email'))
-    serializer_data = {'code': randint(100000, 999999),
-                       'email': email_decode,
-                       'status': 1,
-                       'create': int(time.time())}
-    serializer = EmailCodeSerializer(data=serializer_data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    try:
+        email_decode = urllib.parse.unquote(request.data.get('email'))
+        serializer_data = {'code': randint(100000, 999999),
+                           'email': email_decode,
+                           'status': 1,
+                           'create': int(time.time())}
+        serializer = EmailCodeSerializer(data=serializer_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                {
+                 "code": 200,
+                 "msg": "code generated",
+                 "data": []
+                }
+            )
+        return Response({
+            "code": 10001,
+            "msg": "email code already exist",
+            "data": []
+        })
+    except:
+        return Response({
+            "code": 10001,
+            "msg": "create email code fail",
+            "data": []
+        })
 
 
 @api_view(['POST'])
